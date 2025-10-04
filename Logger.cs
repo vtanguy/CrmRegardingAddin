692d6a5858a773f3f98b2cd3f0d847616bc45ed0
vtanguy <vincent.tanguy@orange.fr>
2025-10-04 21:20:46 +0200

    test branche

--- Logger.cs
+++ Logger.cs
@@ -1,12 +1,29 @@
-﻿using System.Diagnostics;
+using System;
+using System.IO;
+using System.Configuration;
 
 namespace CrmRegardingAddin
 {
-    internal static class Logger
+    public static class Logger
     {
+        private static bool _enabled;
+        private static string _logPath;
+
+        static Logger()
+        {
+            _enabled = ConfigurationManager.AppSettings["CrmAddin:VerboseLog"] == "true";
+            _logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CrmRegardingAddin", "crmaddin.log");
+            Directory.CreateDirectory(Path.GetDirectoryName(_logPath));
+        }
+
         public static void Info(string message)
         {
-            Debug.WriteLine("[CRMADDIN] " + message);
+            if (!_enabled) return;
+            try
+            {
+                File.AppendAllText(_logPath, DateTime.Now.ToString("u") + " " + message + Environment.NewLine);
+            }
+            catch { }
         }
     }
 }
